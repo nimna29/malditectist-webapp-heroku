f30e820c4114bffbee75965f0b7d3f64fafce5fc
Nimna Niwarthana <77341013+Nimna29@users.noreply.github.com>
2023-03-26 00:29:01 +0530

    Update settings

--- malditectist/settings.py
+++ malditectist/settings.py
@@ -60,6 +60,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = "malditectist.urls"
