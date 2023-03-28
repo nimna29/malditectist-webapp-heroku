dbe6eb5f8c0ca07b7f7b76425c5a7372eb2f3969
Nimna Niwarthana <77341013+Nimna29@users.noreply.github.com>
2023-03-28 06:47:20 +0530

    Modify for handle timeout

--- firebase.py
+++ firebase.py
@@ -2,24 +2,29 @@ import os
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import storage
+from dotenv import load_dotenv
 
-# Get Firebase service account credentials from environment variables
-cred = credentials.Certificate({
-    'type': 'service_account',
-    'project_id': os.environ.get('FIREBASE_PROJECT_ID'),
-    'private_key_id': os.environ.get('FIREBASE_PRIVATE_KEY_ID'),
-    'private_key': os.environ.get('FIREBASE_PRIVATE_KEY').replace('\\n', '\n'),
-    'client_email': os.environ.get('FIREBASE_CLIENT_EMAIL'),
-    'client_id': os.environ.get('FIREBASE_CLIENT_ID'),
-    'auth_uri': os.environ.get('FIREBASE_AUTH_URI'),
-    'token_uri': os.environ.get('FIREBASE_TOKEN_URI'),
-    'auth_provider_x509_cert_url': os.environ.get('FIREBASE_AUTH_PROVIDER_CERT_URL'),
-    'client_x509_cert_url': os.environ.get('FIREBASE_CLIENT_CERT_URL')
-})
+load_dotenv()  # Load environment variables from the .env file
+
+
+# Read environment variables
+service_account_data = {
+    "type": "service_account",
+    "project_id": os.environ.get("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.environ.get("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.environ.get("FIREBASE_PRIVATE_KEY").replace('\\n', '\n'),
+    "client_email": os.environ.get("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.environ.get("FIREBASE_CLIENT_ID"),
+    "auth_uri": os.environ.get("FIREBASE_AUTH_URI"),
+    "token_uri": os.environ.get("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.environ.get("FIREBASE_AUTH_PROVIDER_CERT_URL"),
+    "client_x509_cert_url": os.environ.get("FIREBASE_CLIENT_CERT_URL"),
+}
 
 # Initialize the Firebase app with the service account credentials
+cred = credentials.Certificate(service_account_data)
 firebase_admin.initialize_app(cred, {
-    'storageBucket': os.environ.get('FIREBASE_STORAGE_BUCKET')
+    'storageBucket': os.environ.get("FIREBASE_STORAGE_BUCKET")
 })
 
 # Get a reference to the default bucket
